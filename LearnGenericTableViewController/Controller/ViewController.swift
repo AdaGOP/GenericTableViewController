e6950c437d6dc0c19eb0f515b690fcef87cef4c4
Khoirunnisa' Rizky <khoirunnisa.rizky.n@mail.ugm.ac.id>
2022-09-01 21:13:33 +0700

    Initial Commit

---
+++ LearnGenericTableViewController/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LearnGenericTableViewController
+//
+//  Created by khoirunnisa' rizky noor fatimah on 01/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
