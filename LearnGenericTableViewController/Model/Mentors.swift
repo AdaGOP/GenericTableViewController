1601fd131f10853a5fe23f770a95e8af4f6812c4
Khoirunnisa' Rizky <khoirunnisa.rizky.n@mail.ugm.ac.id>
2022-09-01 21:19:56 +0700

    last minor change

--- LearnGenericTableViewController/Model/Mentors.swift
+++ LearnGenericTableViewController/Model/Mentors.swift
@@ -16,7 +16,7 @@ struct Mentors {
     func generateData() -> [Mentors] {
         let data = [Mentors(name: "Viona", image: "Viona", role: "Technical", description: "This is detail description of Viona. This is detail description of Viona.This is detail description of Viona."),
                     Mentors(name: "Octa", image: "Octa", role: "Technical", description: "This is detail description of Octa. This is detail description of Octa.This is detail description of Octa."),
-                    Mentors(name: "Henri", image: "Henri", role: "Professional Growth",    description: "This is detail description of Henri. This is detail description of Henri.This is detail description of Henri."),
+                    Mentors(name: "Henri", image: "Henri", role: "Professional Growth", description: "This is detail description of Henri. This is detail description of Henri.This is detail description of Henri."),
                     Mentors(name: "Phil", image: "Phil", role: "Design", description: "This is detail description of Phil. This is detail description of Phil.This is detail description of Phil."),
                     Mentors(name: "John", image: "John", role: "Design", description: "This is detail description of John. This is detail descriptiom of John.This is detail description of John."),
                     Mentors(name: "Ica", image: "Ica", role: "Design", description: "This is  detail description of Ica. This is detail description of Ica.This is detail description of Ica.")]
